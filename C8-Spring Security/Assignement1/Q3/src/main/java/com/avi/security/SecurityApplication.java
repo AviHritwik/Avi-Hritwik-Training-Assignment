a685b83593169674a16427b3c4fbe17090f0e76c
Your Name <avihritwik4@gmail.com>
2021-04-01 17:46:15 +0530

    pushing updates in react assignment

---
+++ C8-Spring Security/Assignement1/Q3/src/main/java/com/avi/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.avi.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAss1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAss1Application.class, args);
+	}
+
+}
